d945541d532533011953f6f3365c0922f1d171ab
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2024-01-21 10:46:04 +0100

    Parse bittorrent file

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
+#![feature(slice_split_once)]
 use serde_json;
-use std::env;
+use std::{ env, fs, str };
 
 // Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+fn decode_bencoded_value(encoded_value: &[u8]) -> (serde_json::Value, &[u8]) {
     // If encoded_value starts with a digit, it's a number
     //
     dbg!(&encoded_value);
-    let (head, mut tail) = encoded_value.split_at(1);
-    let head: char = head.chars().next().expect("split_at(1) did not panic so the left part has to be nonempty");
+    let (head, mut tail) = encoded_value.split_first().unwrap();
     let (val, new_tail) = match head {
-        'i' => {
-            // let content = &encoded_value[1..].chars().take_while(|&ch| ch != 'e').collect::<String>();
-            let end_index: usize = tail.find('e').unwrap();
-            let value = tail[..end_index].parse::<i64>().unwrap();
+        b'i' => {
+            // let content = &encoded_value[1..].chars().take_while(|&ch| ch != b'e').collect::<String>();
+            let end_index: usize = tail.iter().position(|&x| x == b'e').unwrap();
+            let value = str::from_utf8(&tail[..end_index]).unwrap().parse::<i64>().unwrap();
             (value.into(), &tail[end_index + 1 ..])
         },
-        'l' => {
+        b'l' => {
             let mut values = Vec::new();
-            while !tail.starts_with('e') {
+            while !tail.starts_with(&[b'e']) {
                 let (val, new_tail) = decode_bencoded_value(tail);
                 tail = new_tail;
                 values.push(val);
             }
             (values.into(), &tail[1..])
         },
-        'd' => {
+        b'd' => {
             let mut values = serde_json::Map::new();
-            while !tail.starts_with('e') {
+            while !tail.starts_with(&[b'e']) {
                 let (key, new_tail) = decode_bencoded_value(tail);
                 let (val, new_tail) = decode_bencoded_value(new_tail);
                 tail = new_tail;
@@ -37,14 +37,14 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
             }
             (values.into(), &tail[1..])
         },
-        '0' ..= '9' => {
+        b'0' ..= b'9' => {
             // Example: "5:hello" -> "hello"
-            let (head, tail) = encoded_value.split_once(':').unwrap();
-            let value_length = head.parse::<usize>().unwrap();
-            let value = tail[.. value_length].to_string();
+            let (head, tail) = encoded_value.split_once(|&x| x == b':').unwrap();
+            let value_length = str::from_utf8(&head).unwrap().parse::<usize>().unwrap();
+            let value = String::from_utf8_lossy(&tail[.. value_length]);
             (value.into(), &tail[value_length ..])
         },
-        _ =>  panic!("Unhandled encoded value: {}", encoded_value)
+        _ =>  panic!("Unhandled encoded value: {:?}", encoded_value)
     };
     dbg!(&val);
     (val, new_tail)
@@ -54,12 +54,18 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
-
-    if command == "decode" {
-        let encoded_value = &args[2];
-        let (decoded_value, _encoded_tail) = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    match command.as_str() {
+        "decode" => {
+            let encoded_value = &args[2];
+            let (decoded_value, _encoded_tail) = decode_bencoded_value(encoded_value.as_bytes());
+            println!("{}", decoded_value.to_string());
+        },
+        "info" => {
+            let torrent_filename = &args[2];
+            let content: &Vec<u8> = &fs::read(torrent_filename).unwrap();
+            let (metainfo, _encoded_tail) = decode_bencoded_value(content);
+            println!("{}", metainfo["info"]["length"]);
+        },
+        _ => println!("unknown command: {}", args[1])
     }
 }
