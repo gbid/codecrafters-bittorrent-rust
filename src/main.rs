1c21878511cfe2ddf16666325c4c849885842228
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2024-01-30 15:12:04 +0100

    Use clap for cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,139 @@
-use serde_bencode;
-use std::{ env, fs };
-use std::net::{ TcpStream, SocketAddr };
-use std::str::FromStr;
-use hex;
+use clap::{ arg, Command, Arg, ArgAction };
+use std::{ fs, str::FromStr};
+use std::net::{ SocketAddr, TcpStream };
 use bittorrent_starter_rust::{ Torrent, bencode, tracker, network };
+use hex;
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-    match command.as_str() {
-        "decode" => {
-            let encoded_value = &args[2];
+    let matches = Command::new("bittorrent-client")
+        .about("A BitTorrent client")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("decode")
+                .about("Decodes a bencoded value")
+                .arg(arg!(<ENCODED_VALUE> "The value to decode")),
+        )
+        .subcommand(
+            Command::new("info")
+                .about("Displays information about a torrent")
+                .arg(arg!(<TORRENT_FILE> "The torrent file")),
+        )
+        .subcommand(
+            Command::new("peers")
+                .about("Lists peers for a torrent")
+                .arg(arg!(<TORRENT_FILE> "The torrent file")),
+        )
+        .subcommand(
+            Command::new("handshake")
+                .about("Performs a handshake with a peer")
+                .arg(arg!(<TORRENT_FILE> "The torrent file"))
+                .arg(arg!(<PEER> "The peer to connect to")),
+        )
+        .subcommand(
+            Command::new("download_piece")
+                .about("Downloads a specific piece of a torrent")
+                .arg(Arg::new("OUTPUT")
+                    .short('o')
+                    .long("output")
+                    .action(ArgAction::Set)
+                    .value_name("OUTPUT")
+                    .help("Output file")
+                    .required(true))
+                .arg(arg!(<TORRENT_FILE> "The torrent file"))
+                .arg(arg!(<PIECE_INDEX> "The index of the piece to download")),
+        )
+        .subcommand(
+            Command::new("download")
+                .about("Downloads the entire torrent")
+                // .arg(arg!(-o --output <OUTPUT> "Output file").required(true))
+                .arg(Arg::new("OUTPUT")
+                    .short('o')
+                    .long("output")
+                    .action(ArgAction::Set)
+                    .value_name("OUTPUT")
+                    .help("Output file")
+                    .required(true))
+                .arg(arg!(<TORRENT_FILE> "The torrent file")),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("decode", sub_matches)) => {
+            let encoded_value = sub_matches.get_one::<String>("ENCODED_VALUE").unwrap();
             let (decoded_value, _encoded_tail) = bencode::decode_bencoded_value(encoded_value.as_bytes());
             println!("{}", decoded_value.to_string());
         },
-        "info" => {
-            let torrent_filename = &args[2];
-            let content: Vec<u8> = fs::read(torrent_filename).unwrap();
+        Some(("info", sub_matches)) => {
+            let torrent_filename = sub_matches.get_one::<String>("TORRENT_FILE").unwrap();
+            let content = fs::read(torrent_filename).unwrap();
             let torrent = Torrent::from_bytes(&content);
-            println!("Tracker URL: {}", torrent.announce);
-            println!("Length: {}", torrent.info.length);
-            let info_hash = torrent.get_info_hash();
-            println!("Info Hash: {}", hex::encode(&info_hash));
-            println!("Piece Length: {}", torrent.info.piece_length);
-            println!("Piece Hashes:");
-            for piece in torrent.info.pieces.chunks(20) {
-                println!("{}", hex::encode(piece));
-            }
+            display_torrent_info(&torrent);
         },
-        "peers" => {
-            let torrent_filename = &args[2];
-            let content: Vec<u8> = fs::read(torrent_filename).unwrap();
-            let torrent: Torrent = serde_bencode::from_bytes(&content).unwrap();
+        Some(("peers", sub_matches)) => {
+            let torrent_filename = sub_matches.get_one::<String>("TORRENT_FILE").unwrap();
+            let content = fs::read(torrent_filename).unwrap();
+            let torrent = Torrent::from_bytes(&content);
             let tracker_response = tracker::get_tracker(&torrent);
             for peer in tracker_response.peers {
                 println!("{}", peer);
             }
         },
-        "handshake" => {
-            let torrent_filename = &args[2];
-            let content: &Vec<u8> = &fs::read(torrent_filename).unwrap();
-            let torrent: Torrent = serde_bencode::from_bytes(content).unwrap();
-            let peer = SocketAddr::from_str(&args[3]).unwrap();
-            let stream = TcpStream::connect(&peer).unwrap();
-            let response_peer_id = network::perform_peer_handshake(&torrent, &stream);
-            println!("Peer ID: {}", hex::encode(&response_peer_id));
+        Some(("handshake", sub_matches)) => {
+            let torrent_filename = sub_matches.get_one::<String>("TORRENT_FILE").unwrap();
+            let peer_address = sub_matches.get_one::<String>("PEER").unwrap();
+            perform_handshake(torrent_filename, peer_address);
         },
-        "download_piece" => {
-            // your_bittorrent.sh download_piece -o /tmp/test.txt sample.torrent 0
-            // TODO: properly parse "-o" cli option
-            let output_filename = &args[3];
-            let torrent_filename = &args[4];
-            let content: &Vec<u8> = &fs::read(torrent_filename).unwrap();
-            let torrent = Torrent::from_bytes(&content);
-            let piece_index = u32::from_str(&args[5]).unwrap();
-            let downloaded_piece: Vec<u8> = network::download_piece(&torrent, piece_index);
-            assert!(torrent.is_piece_hash_correct(&downloaded_piece, piece_index));
-
-            fs::write(output_filename, downloaded_piece).unwrap();
-            println!("Piece {} downloaded to {}.", piece_index, output_filename);
+        Some(("download_piece", sub_matches)) => {
+            let output_filename = sub_matches.get_one::<String>("OUTPUT").unwrap();
+            let torrent_filename = sub_matches.get_one::<String>("TORRENT_FILE").unwrap();
+            let piece_index = sub_matches.get_one::<String>("PIECE_INDEX").unwrap().parse::<u32>().unwrap();
+            download_single_piece(torrent_filename, output_filename, piece_index);
         },
-        "download" => {
-            // your_bittorrent.sh download -o /tmp/test.txt sample.torrent
-            // TODO: properly parse "-o" cli option
-            let output_filename = &args[3];
-            let torrent_filename = &args[4];
-            let content: Vec<u8> = fs::read(torrent_filename).unwrap();
-            let torrent = Torrent::from_bytes(&content);
-            let downloaded_pieces: Vec<u8> = network::download_and_verify_pieces(&torrent);
-            fs::write(output_filename, &downloaded_pieces).unwrap();
-
-            println!("Downloaded {} to {}", torrent_filename, output_filename);
+        Some(("download", sub_matches)) => {
+            dbg!(&sub_matches);
+            let output_filename = sub_matches.get_one::<String>("OUTPUT").unwrap();
+            let torrent_filename = sub_matches.get_one::<String>("TORRENT_FILE").unwrap();
+            download_torrent(torrent_filename, output_filename);
         },
-        _ => println!("unknown command: {}", args[1])
+        _ => unreachable!(),
     }
+}
+
+fn display_torrent_info(torrent: &Torrent) {
+    println!("Tracker URL: {}", torrent.announce);
+    println!("Length: {}", torrent.info.length);
+    let info_hash = torrent.get_info_hash();
+    println!("Info Hash: {}", hex::encode(&info_hash));
+    println!("Piece Length: {}", torrent.info.piece_length);
+    println!("Piece Hashes:");
+    for piece in torrent.info.pieces.chunks(20) {
+        println!("{}", hex::encode(piece));
+    }
+}
+
+fn perform_handshake(torrent_filename: &str, peer_address: &str) {
+    let content = fs::read(torrent_filename).unwrap();
+    let torrent: Torrent = serde_bencode::from_bytes(&content).unwrap();
+    let peer = SocketAddr::from_str(peer_address).unwrap();
+    let stream = TcpStream::connect(&peer).unwrap();
+    let response_peer_id = network::perform_peer_handshake(&torrent, &stream);
+    println!("Peer ID: {}", hex::encode(&response_peer_id));
+}
+
+fn download_single_piece(torrent_filename: &str, output_filename: &str, piece_index: u32) {
+    let content = fs::read(torrent_filename).unwrap();
+    let torrent = Torrent::from_bytes(&content);
+    let downloaded_piece: Vec<u8> = network::download_piece(&torrent, piece_index);
+    assert!(torrent.is_piece_hash_correct(&downloaded_piece, piece_index));
+    fs::write(output_filename, downloaded_piece).unwrap();
+    println!("Piece {} downloaded to {}.", piece_index, output_filename);
+}
+
+fn download_torrent(torrent_filename: &str, output_filename: &str) {
+    let content = fs::read(torrent_filename).unwrap();
+    let torrent = Torrent::from_bytes(&content);
+    let downloaded_pieces: Vec<u8> = network::download_and_verify_pieces(&torrent);
+    fs::write(output_filename, &downloaded_pieces).unwrap();
+    println!("Downloaded {} to {}", torrent_filename, output_filename);
 }
